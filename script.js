8d6e314101215e1eb4fa30c99b2465a3b461c595
himanshuhsn <himanshu000666@gmail.com>
2025-03-15 01:37:03 +0530

    fix: remove video player from auto closing. it never worked.

--- script.js
+++ script.js
@@ -308,15 +308,4 @@ function showFloatingVideo() {
   setInterval(moveVideoRandomly, 5000);
   // Initial random placement
   moveVideoRandomly();
-
-  // The API calls this function when the player's state changes
-  function onPlayerStateChange(event) {
-    if (event.data === YT.PlayerState.ENDED) {
-      // Remove the floating video div when the video ends
-      const videoContainer = document.getElementById("floating-video");
-      if (videoContainer) {
-        videoContainer.remove();
-      }
-    }
-  }
 }
